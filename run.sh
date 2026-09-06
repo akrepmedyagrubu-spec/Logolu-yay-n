9ae0ede754a0ec43e18cf9043ae04d680456a502
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-09-06 22:19:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,17 @@
 
 mkdir -p /app/hls
 
-# 🔥 BURAYA KENDİ M3U8 LİNKİNİ KOY
 INPUT_STREAM="https://corestream.ardastream.live//beintv/tracks-v1a1/mono.m3u8"
 
 echo "FFmpeg başlatılıyor..."
 
-# 🔁 Sürekli çalış (çökse bile tekrar başlar)
 (
 while true
 do
   ffmpeg -loglevel warning \
   -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
   -i "$INPUT_STREAM" -i logo.png \
-  -filter_complex "[1:v]scale=main_w:main_h[logo];[0:v][logo]overlay=0:0" \
+  -filter_complex "[1:v][0:v]scale2ref=w=iw:h=ih[logo][base];[base][logo]overlay=0:0" \
   -c:v libx264 -preset veryfast -crf 23 \
   -c:a aac -b:a 128k \
   -f hls \
@@ -27,9 +25,6 @@ do
   sleep 3
 done
 ) &
-
-# 🌐 HTTP SERVER (Render port fix)
-echo "Server başlatılıyor..."
 
 cd /app
 python3 -m http.server ${PORT:-10000}
